4b2ee1a74515dedb265dd195e6a61d2ac1f8ea3b
smendimasa <sam34@umbc.edu>
2018-04-07 14:46:37 -0400

    thanks katelyn

--- index.php
+++ index.php
@@ -40,40 +40,73 @@
         overlayBg.style.display = "none";
     }
 
-    function initMap() {
-    	var map = new google.maps.Map(document.getElementById('map'), {
-    		zoom: 11,
-    		center: {lat: 39.2904, lng: -76.6122}
-    	});
 
-    	// Create an array of alphabetical characters used to label the markers.
-    	var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    	// Add some markers to the map.
-    	// Note: The code uses the JavaScript Array.prototype.map() method to
-    	// create an array of markers based on a given "locations" array.
-    	// The map() method here has nothing to do with the Google Maps API.
-    	var markers = locations.map(function(location, i) {
-    		return new google.maps.Marker({
-    			position: location,
-    			label: labels[i % labels.length]
-    			});
-    		});
-    	// Add a marker clusterer to manage the markers.
-    	var markerCluster = new MarkerClusterer(map, markers,
-    		{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+</script>
+
+<script type="text/javascript">
+//KS
+function initMap() {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 11,
+		center: {lat: 39.2904, lng: -76.6122}
+	});
+
+	// Create an array of alphabetical characters used to label the markers.
+	//var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	
+	// Set body of tooltip
+	var contentString = '<div id="content">'+
+		'<div id="siteNotice">'+
+		'</div>'+
+		'<h1 id="firstHeading" class="firstHeading">CRIME</h1>'+
+		'<div id="bodyContent">'+
+		'<p><b>Crime</b> was commited here at <b>time</b> on <b>date</b>.</p>'+
+		'<p>for full entry, click <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+		'here</a></p> '+
+		'<p><button onclick="addComment()">Add comment</button></p>' +
+		'</div>'+
+		'</div>';
+
+	// Create tooltip that will appear when marker is clicked
+	var tooltip = new google.maps.InfoWindow({
+		content: contentString
+		});
+	// Add some markers to the map.
+	// Note: The code uses the JavaScript Array.prototype.map() method to
+	// create an array of markers based on a given "locations" array.
+	// The map() method here has nothing to do with the Google Maps API.
+	var markers = locations.map(function(location, i) {
+		var marker = new google.maps.Marker({
+			position: location,
+			map: map,
+			//label: labels[i % labels.length]
+			});
+		marker.addListener('click', function() {
+			tooltip.open(map, marker);
+		});
+		return marker
+		});
+	// Add a marker clusterer to manage the markers.
+	var markerCluster = new MarkerClusterer(map, markers,
+		{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+}//KS
+var locations = [
+// Get {lat, long} of all selected crimes using SQL
+{lat: 39.2904, lng: -76.6122},
+{lat: 39.3, lng: -76.6},
+{lat: 39.3, lng: -76.6122},
+{lat: 39.2904, lng: -76.6}
+]
+
+//add in data
+function addComment() {
+    var user = "USERNAME";
+    var comment = prompt("Enter comment:", "");
+    if (comment != null && comment != "") {
+        //<user> added comment to <crime entry>
     }
-    var locations = [
-    // Get {lat, long} of all selected crimes using SQL
-    {lat: 39.2904, lng: -76.6122},
-    {lat: 39.2904, lng: -76.6122},
-    {lat: 39.2904, lng: -76.6122},
-    {lat: 39.2904, lng: -76.6122},
-    {lat: 39.2904, lng: -76.6122},
-    {lat: 39.35, lng: -76.6122},
-    {lat: 39.35, lng: -76.6122},
-    {lat: 39.35, lng: -76.6122}
-    ]
+}
 
 </script>
 
