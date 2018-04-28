efff4b242cd27a5447f23a4588f836c148e31e81
Zach Vance <ZachV@Zachs-MacBook-Pro.local>
2018-04-27 22:02:50 -0400

    added date filter

--- db/getMapLocations.php
+++ db/getMapLocations.php
@@ -39,6 +39,22 @@ $northern = ($_POST['wt_NorthWestern1']);
 $eastern = ($_POST['wt_SouthEastern1']);
 $northWestern = ($_POST['wt_SouthWestern1']);
 
+
+//date / time
+
+$startDate = ($_POST['wt_StartDate1']);
+$startDate = date("$startDate");
+
+$endDate = ($_POST['wt_EndDate1']);
+$endDate = date("$endDate");
+
+$startTime = ($_POST['wt_StartTime1']);
+$startTime = date("$startTime");
+
+$endTime = ($_POST['wt_EndTime1']);
+$endTime = date("$endTime");
+
+
 $db = new SQLite3('mydb.db');
 $myStreet="STREET";
 //$results = $db->query("SELECT * FROM mydb WHERE premise='$myStreet'");
@@ -58,6 +74,7 @@ district = '$southEastern' or district = '$southern' or district = '$northern' o
   '$shooting') and (weapon ='$other'or  weapon = '$hands' or weapon ='$knife' or weapon ='$firearm')");
   */
 if($none == "NONE"){
+	/*
   $results = $db->query("
   SELECT sub2.* 
   
@@ -80,29 +97,110 @@ if($none == "NONE"){
 WHERE (sub2.district ='$central' or sub2.district = '$western' or sub2.district = '$northEastern' or sub2.district = '$southWestern' or 
 sub2.district = '$southEastern' or sub2.district = '$southern' or sub2.district = '$northern' or sub2.district = '$eastern'
  or sub2.district = '$northWestern' or sub2.district IS NULL);");
+ */
+ 
+ $results = $db->query("
+ SELECT sub3.*
+  
+  
+  		FROM (SELECT sub2.* 
+  
+  					FROM ( SELECT sub.* 
+  
+  								FROM (
+  			 								SELECT * 
+  			 								FROM mydb 
+  			 								WHERE(weapon ='$other'or  weapon = '$hands' or weapon 
+  					 						='$knife' or weapon ='$firearm' or weapon IS NULL)
+  											)sub
+  			
+  									WHERE (sub.description = '$aggAssault' or sub.description = '$arson' or sub.description
+  									= '$assaultByThreat' or sub.description = '$autoTheft' or sub.description = '$burglary' or sub.description = 
+  									'$commonAssault' or sub.description = '$homicide' or sub.description = '$larceny' or sub.description = 
+  									'$larcenyAuto' or sub.description = '$rape' or sub.description = '$robberyStreet' or sub.description = 
+  									'$robberyCar' or sub.description = '$robberyCom' or sub.description = '$robberyRes' or sub.description = 
+  									'$shooting' or sub.description IS NULL)
+  							)sub2
+								WHERE (sub2.district ='$central' or sub2.district = '$western' or sub2.district = '$northEastern' or sub2.district = '$southWestern' or 
+								sub2.district = '$southEastern' or sub2.district = '$southern' or sub2.district = '$northern' or sub2.district = '$eastern'
+ 								or sub2.district = '$northWestern' or sub2.district IS NULL)
+ 			
+ 							)sub3
+ 			
+			WHERE(crimedate >= '$startDate' and crimedate <= '$endDate' )
+			;");
+			
+			
+ /*
+ $results = $db->query("
+ SELECT sub4.*
+ 
+ FROM (SELECT sub3.*
+  
+  
+  		FROM (SELECT sub2.* 
+  
+  					FROM ( SELECT sub.* 
+  
+  								FROM (
+  			 								SELECT * 
+  			 								FROM mydb 
+  			 								WHERE(weapon ='$other'or  weapon = '$hands' or weapon 
+  					 						='$knife' or weapon ='$firearm' or weapon IS NULL)
+  											)sub
+  			
+  									WHERE (sub.description = '$aggAssault' or sub.description = '$arson' or sub.description
+  									= '$assaultByThreat' or sub.description = '$autoTheft' or sub.description = '$burglary' or sub.description = 
+  									'$commonAssault' or sub.description = '$homicide' or sub.description = '$larceny' or sub.description = 
+  									'$larcenyAuto' or sub.description = '$rape' or sub.description = '$robberyStreet' or sub.description = 
+  									'$robberyCar' or sub.description = '$robberyCom' or sub.description = '$robberyRes' or sub.description = 
+  									'$shooting' or sub.description IS NULL)
+  							)sub2
+								WHERE (sub2.district ='$central' or sub2.district = '$western' or sub2.district = '$northEastern' or sub2.district = '$southWestern' or 
+								sub2.district = '$southEastern' or sub2.district = '$southern' or sub2.district = '$northern' or sub2.district = '$eastern'
+ 								or sub2.district = '$northWestern' or sub2.district IS NULL)
+ 			
+ 							)sub3
+ 			
+			WHERE(crimedate >= '$startDate' and crimedate <= '$endDate' )
+			
+			)sub4
+		
+	WHERE(crimetime >= '$startTime' and crimetime <= '$endTime')
+ 					;");
+ */
 }else{
 	$results = $db->query("
-  SELECT sub2.* 
+	
+	SELECT sub3.*
   
-  FROM ( SELECT sub.* 
   
-  			FROM (
-  			 		SELECT * 
-  			 		FROM mydb 
-  			 		WHERE(weapon ='$other'or  weapon = '$hands' or weapon 
-  					 ='$knife' or weapon ='$firearm')
-  			)sub
+  FROM (SELECT sub2.* 
+  
+ 				 FROM ( SELECT sub.* 
+  
+  						FROM (
+  			 						SELECT * 
+  			 						FROM mydb 
+  			 						WHERE(weapon ='$other'or  weapon = '$hands' or weapon 
+  									 ='$knife' or weapon ='$firearm')
+  							)sub
   			
-  			WHERE (sub.description = '$aggAssault' or sub.description = '$arson' or sub.description
-  			= '$assaultByThreat' or sub.description = '$autoTheft' or sub.description = '$burglary' or sub.description = 
-  			'$commonAssault' or sub.description = '$homicide' or sub.description = '$larceny' or sub.description = 
-  			'$larcenyAuto' or sub.description = '$rape' or sub.description = '$robberyStreet' or sub.description = 
-  			'$robberyCar' or sub.description = '$robberyCom' or sub.description = '$robberyRes' or sub.description = 
-  			'$shooting' or sub.description IS NULL)
-  )sub2
-WHERE (sub2.district ='$central' or sub2.district = '$western' or sub2.district = '$northEastern' or sub2.district = '$southWestern' or 
-sub2.district = '$southEastern' or sub2.district = '$southern' or sub2.district = '$northern' or sub2.district = '$eastern'
- or sub2.district = '$northWestern' or sub2.district IS NULL);");
+  							WHERE (sub.description = '$aggAssault' or sub.description = '$arson' or sub.description
+  							= '$assaultByThreat' or sub.description = '$autoTheft' or sub.description = '$burglary' or sub.description = 
+  							'$commonAssault' or sub.description = '$homicide' or sub.description = '$larceny' or sub.description = 
+  							'$larcenyAuto' or sub.description = '$rape' or sub.description = '$robberyStreet' or sub.description = 
+  							'$robberyCar' or sub.description = '$robberyCom' or sub.description = '$robberyRes' or sub.description = 
+  							'$shooting' or sub.description IS NULL)
+ 				 )sub2
+				WHERE (sub2.district ='$central' or sub2.district = '$western' or sub2.district = '$northEastern' or sub2.district = '$southWestern' or 
+				sub2.district = '$southEastern' or sub2.district = '$southern' or sub2.district = '$northern' or sub2.district = '$eastern'
+				 or sub2.district = '$northWestern' or sub2.district IS NULL)
+				 
+				 )sub3
+ 			
+			WHERE(crimedate >= '$startDate' and crimedate <= '$endDate' )
+			;");
 }
   
   /*
